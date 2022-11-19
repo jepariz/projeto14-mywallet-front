825a45f0e84d8aa30fbb32a1a4a42b8e7e7e1deb
jepariz <je.parizotto@gmail.com>
2022-11-19 12:28:44 -0300

    criei a função de login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import GlobalStyle from './GlobalStyle'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+import MyContext from "./contexts/MyContext";
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Wallet from "./pages/Wallet"
@@ -9,10 +11,18 @@ import NewEntry from './pages/NewEntry';
 
 function App() {
 
+  const [token, setToken] = useState("");
+
   return (
     <>
     <BrowserRouter>
     <GlobalStyle />
+    <MyContext.Provider
+          value={{
+            token,
+            setToken,
+          }}
+        ></MyContext.Provider>
     <Routes>
       <Route path='/' element={<Login />}/>
       <Route path='/registro' element={ <Register/>}/>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
-import React from "react";
 import styled from "styled-components";
 import { useNavigate, Link } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
+import axios from "axios";
+import { useState, useContext } from "react";
+import MyContext from "../contexts/MyContext";
 
 export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
+
+  const {setToken } = useContext(MyContext);
+  const navigate = useNavigate();
+
+  function handleLogin() {
+    const promise = axios.post(
+      "",
+      {
+        email: email,
+        password: password,
+      }
+    );
+    promise.then((res) => {
+      console.log(res.data);
+      setToken(res.data.token)
+      navigate("/carteira");
+    });
+
+    promise.catch((err) => {
+      alert(err.response.data.message);
+      setLogin(false);
+    });
+
+    setLogin(true);
+  }
+
   return (
     <LoginContainer>
       <div>
@@ -10,11 +43,41 @@ export default function Login() {
         <h1>My Wallet</h1>
       </div>
       <LoginForm>
-        <input type="email" placeholder="E-mail"></input>
-        <input type="password" placeholder="Senha"></input>
-        <button>Entrar</button>
+        <input
+          type="email"
+          placeholder="E-mail"
+          disabled={login ? true : false}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></input>
+        <input
+          type="password"
+          placeholder="Senha"
+          disabled={login ? true : false}
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></input>
+        {login ? (
+          <button>
+            <ThreeDots
+              height="60"
+              width="60"
+              radius="9"
+              color="#fff"
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{}}
+              wrapperClassName=""
+              visible={true}
+            />
+          </button>
+        ) : (
+          <button onClick={handleLogin}>Entrar</button>
+        )}
       </LoginForm>
-      <Link to="/registro" style={{textDecoration: 'none'}}> <p>Primeira vez? Cadastre-se!</p></Link>
+      <Link to="/registro" style={{ textDecoration: "none" }}>
+        {" "}
+        <p>Primeira vez? Cadastre-se!</p>
+      </Link>
     </LoginContainer>
   );
 }
@@ -58,7 +121,7 @@ const LoginForm = styled.form`
     border: none;
     border-radius: 5px;
     box-sizing: border-box;
-    padding: 5px;
+    padding: 10px;
     margin-bottom: 10px;
     color: #a0a5ba;
     font-size: 20px;
@@ -92,9 +155,4 @@ const LoginForm = styled.form`
     border: 1px solid rgba(255, 255, 255, 0.3);
     margin-bottom: 20px;
   }
-
-
 `;
-
-
-
